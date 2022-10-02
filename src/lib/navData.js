5d188ad6c1f5d7b2929122506b87ae34f74bbd32
sz-chi <judy881204@gmail.com>
2022-10-02 15:31:43 +0800

    second commit

--- src/lib/navData.js
+++ src/lib/navData.js
@@ -1,22 +1,32 @@
+import HomeIcon from '@mui/icons-material/Home';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+ 
 export const navData = [
     {
         id: 0,
+        icon: <HomeIcon/>,
         text: "Home",
         link: "/"
     },
     {
         id: 1,
-        text: "Tableware",
-        link: "tableware"
+        icon: <TravelExploreIcon/>,
+        text: "Explore",
+        link: "explore"
     },
     {
         id: 2,
-        text: "Cookware",
-        link: "cookware"
+        icon: <BarChartIcon/>,
+        text: "Statistics",
+        link: "statistics"
     },
     {
         id: 3,
-        text: "Product",
-        link: "product/:productID"
+        icon: <SettingsIcon/>,
+        text: "Settings",
+        link: "settings"
     }
 ]
